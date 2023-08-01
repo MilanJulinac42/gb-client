8c53b95f38e744cc5db13040eca3cf9bf8765343
milan <milanjulinac996@gmail.com>
2023-08-01 16:05:22 +0200

    gb-67 added fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Karla, Zilla_Slab } from "@next/font/google";
+
+const karla = Karla({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+});
+
+const zilla = Zilla_Slab({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<main className={zilla.className}>
+			{" "}
+			<Component {...pageProps} />;
+		</main>
+	);
 }
 
-export default MyApp
+export default MyApp;

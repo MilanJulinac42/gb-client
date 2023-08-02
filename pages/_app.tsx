3e1d464cdfb43b6189a421483c81bccf3c124e4e
milan <milanjulinac996@gmail.com>
2023-08-02 09:38:31 +0200

    gb-70 font size mixin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,27 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Karla, Zilla_Slab } from "@next/font/google";
+import { Karla, Zilla_Slab } from "next/font/google";
 
-const karla = Karla({
+export const karla = Karla({
 	subsets: ["latin"],
 	weight: ["400", "700"],
+	variable: "--font-karla",
 });
 
-const zilla = Zilla_Slab({
+export const zilla = Zilla_Slab({
 	subsets: ["latin"],
 	weight: ["400", "700"],
+	variable: "--font-zilla",
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<main className={zilla.className}>
-			{" "}
-			<Component {...pageProps} />;
-		</main>
+		<>
+			<main className={karla.className}>
+				{" "}
+				<Component {...pageProps} />;
+			</main>
+		</>
 	);
 }
 

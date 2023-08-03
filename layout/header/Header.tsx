572e05c2d77282741a909b1218a57dc859c216fd
milan <milanjulinac996@gmail.com>
2023-08-03 10:28:15 +0200

    gb-72 added header layout

---
+++ layout/header/Header.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Logo from "../../components/logo/Logo";
+import styles from "./Header.module.scss";
+import { zilla } from "../../pages/_app";
+
+const Header: React.FC = () => {
+	return (
+		<header className={styles.header}>
+			<Logo></Logo>
+			<nav className={styles.header__navigation}>
+				<ul>
+					<li>
+						<Link href={"/"}>Home</Link>
+					</li>
+					<li>
+						<Link href={"/products"}>Products</Link>
+					</li>
+					<li>
+						<Link href={"/about"}>About</Link>
+					</li>
+					<li>
+						<Link href={"/testimonials"}>Testimonials</Link>
+					</li>
+				</ul>
+			</nav>
+			<div className={styles.header__auth}>
+				<Link href={"/login"}>Login</Link>
+				<Link href={"/register"}>Register</Link>
+			</div>
+		</header>
+	);
+};
+
+export default Header;

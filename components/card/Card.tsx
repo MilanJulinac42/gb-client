45dfa490fe7a34e7a46b4b06cc546f9b0b470b9e
MilanJulinac42 <milanjulinac996@gmail.com>
2023-09-27 15:47:57 +0200

    gb-76 added gallery

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import styles from "./Card.module.scss"
+import styles from "./Card.module.scss";
 
-const Card: React.FC = () => {
-    return (
-        <div></div>
-    )
+interface ProductCardProps {
+  name: string;
+  price: number;
+  imageUrl: string;
 }
+
+const Card: React.FC<ProductCardProps> = ({ name, price, imageUrl }) => {
+  return (
+    <div className={styles.productCard}>
+      <img src={imageUrl} alt={name} className={styles.productImage} />
+      <div className={styles.productInfo}>
+        <h3 className={styles.productName}>{name}</h3>
+        <p className={styles.productPrice}>${price.toFixed(2)}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;

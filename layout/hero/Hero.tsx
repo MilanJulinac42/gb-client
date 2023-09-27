d93cd0f1996b521ea348631ecd31ffd81a955859
MilanJulinac42 <milanjulinac996@gmail.com>
2023-09-27 14:54:31 +0200

    gb-77 reworked

--- layout/hero/Hero.tsx
+++ layout/hero/Hero.tsx
@@ -1,19 +1,33 @@
 import React from "react";
 import styles from "./Hero.module.scss";
 import HeroImage from "../../components/heroImage/HeroImage";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleDoubleDown,
+  faAngleDown,
+  faChevronDown,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Hero: React.FC = () => {
-	return (
-		<div className={styles.wrapper}>
-			<h1 className={styles.heading}>Cinimo trenutke posebnim</h1>
-			<button className={styles.cta}>SHOP NOW</button>
-			<div className={styles.imageWrapper}>
-				<HeroImage src="/gb1.jpg" />
-				<HeroImage src="/gb2.jpeg" />
-				<HeroImage src="/gb3.jpeg" />
-			</div>
-		</div>
-	);
+  return (
+    // <div className={styles.wrapper}>
+    // 	<h1 className={styles.heading}>Cinimo trenutke posebnim</h1>
+    // 	<button className={styles.cta}>SHOP NOW</button>
+    // 	<div className={styles.imageWrapper}>
+    // 		<HeroImage src="/gb1.jpg" />
+    // 		<HeroImage src="/gb2.jpeg" />
+    // 		<HeroImage src="/gb3.jpeg" />
+    // 	</div>
+    // </div>
+    <div className={styles.wrapperT}>
+      <div className={styles.contentWrapper}>
+        <h1 className={styles.h1T}>Cinimo trenutke posebnim</h1>
+        <button className={styles.ctaT}>SHOP NOW</button>
+        <div className={styles.scroll}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;

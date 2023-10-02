a95cc87e4b6644c077680fbe8144457c02322697
MilanJulinac42 <milanjulinac996@gmail.com>
2023-10-02 22:50:17 +0200

    gb-80 added alternative to gallery cards

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -13,23 +13,45 @@ interface ProductCardProps {
 
 const Card: React.FC<ProductCardProps> = ({ name, price, imageUrl }) => {
   return (
+    // <div className={styles.container}>
+    //   <Image src={imageUrl} alt="random" width={300} height={300}/>
+    //   <div className={styles.overlay}>
+    //     <div className={styles.items}></div>
+    //     <div className={classNames(styles.items, styles.head)}>
+    //       <p>{name}</p>
+    //       <hr />
+    //     </div>
+    //     <div className={classNames(styles.items, styles.price)}>
+    //       <p className={styles.new}>{price}$</p>
+    //     </div>
+    //     <div className={classNames(styles.items, styles.cart)}>
+    //       <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
+    //       <span>ADD TO CART</span>
+    //     </div>
+    //   </div>
+    // </div>
+    // ### ALT ###
     <div className={styles.container}>
-      <Image src={imageUrl} alt="random" width={300} height={300}/>
-      <div className={styles.overlay}>
-        <div className={styles.items}></div>
-        <div className={classNames(styles.items, styles.head)}>
-          <p>{name}</p>
-          <hr />
+    <div className={styles.card}>
+        <div className={styles.imgBx}>
+        <Image src={imageUrl} alt="random" width={300} height={300}/>
         </div>
-        <div className={classNames(styles.items, styles.price)}>
-          <p className={styles.new}>{price}$</p>
+
+        <div className={styles.contentBx}>
+
+            <h2>Nike Shoes</h2>
+
+            <div className={styles.size}>
+                <h3>Price :</h3>
+                <span>{price}</span>
+            </div>
+
+           
+            <a href="#">Buy Now</a>
         </div>
-        <div className={classNames(styles.items, styles.cart)}>
-          <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
-          <span>ADD TO CART</span>
-        </div>
-      </div>
+
     </div>
+</div>
   );
 };
 

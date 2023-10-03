2880917824d111fc4b88e422fa92be20fbe8d98e
MilanJulinac42 <milanjulinac996@gmail.com>
2023-10-03 09:54:39 +0200

    gb-83 deleted unused code in card component

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./Card.module.scss";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import classNames from "classnames";
 
 interface ProductCardProps {
   name: string;
@@ -13,24 +10,6 @@ interface ProductCardProps {
 
 const Card: React.FC<ProductCardProps> = ({ name, price, imageUrl }) => {
   return (
-    // <div className={styles.container}>
-    //   <Image src={imageUrl} alt="random" width={300} height={300}/>
-    //   <div className={styles.overlay}>
-    //     <div className={styles.items}></div>
-    //     <div className={classNames(styles.items, styles.head)}>
-    //       <p>{name}</p>
-    //       <hr />
-    //     </div>
-    //     <div className={classNames(styles.items, styles.price)}>
-    //       <p className={styles.new}>{price}$</p>
-    //     </div>
-    //     <div className={classNames(styles.items, styles.cart)}>
-    //       <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
-    //       <span>ADD TO CART</span>
-    //     </div>
-    //   </div>
-    // </div>
-    // ### ALT ###
     <div className={styles.container}>
       <div className={styles.card}>
         <div className={styles.imgBx}>

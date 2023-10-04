a392bd6c5d77fdd28fbb0f7da3d4d1274d11c47c
MilanJulinac42 <milanjulinac996@gmail.com>
2023-10-04 14:22:17 +0200

    gb-86 landing page gallery is responsive

--- layout/landingPageGallery/LandingPageGallery.tsx
+++ layout/landingPageGallery/LandingPageGallery.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./LandingPageGallery.module.scss";
 import Card from "../../components/card/Card";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useMediaQuery } from "react-responsive";
 
 const LandingPageGallery: React.FC = () => {
   const products = [
@@ -10,16 +14,60 @@ const LandingPageGallery: React.FC = () => {
     { name: "Product 4", price: 54.99, imageUrl: "/gb1.jpg" },
     { name: "Product 5", price: 64.99, imageUrl: "/gb1.jpg" },
   ];
+
+  const isSmallScreen = useMediaQuery({ query: "(max-width: 768px)" });
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "60px",
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className={styles.wrapper}>
-      <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi tempora fuga corrupti ullam odio eos dolorem ducimus at eligendi! Earum consectetur corrupti doloribus repudiandae, ex quae eos sit? Delectus minima facilis harum repellat nulla dolores illo reiciendis nihil cumque, amet laudantium et unde beatae enim excepturi ut iusto voluptates placeat.</p>
-      <div
-        className={styles.gallery}
-      >
-        {products.map((product, index) => (
-          <Card key={index} {...product} />
-        ))}
-      </div>
+    <div className={styles.sliderWrapper}>
+      {isSmallScreen ? (
+        <Slider {...settings}>
+          {products.map((product, index) => (
+            <div key={index}>
+              <Card {...product} />
+            </div>
+          ))}
+        </Slider>
+      ) : (
+        <div className={styles.wrapper}>
+          <p className={styles.text}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
+            tempora fuga corrupti ullam odio eos dolorem ducimus at eligendi!
+            Earum consectetur corrupti doloribus repudiandae, ex quae eos sit?
+            Delectus minima facilis harum repellat nulla dolores illo reiciendis
+            nihil cumque, amet laudantium et unde beatae enim excepturi ut iusto
+            voluptates placeat.
+          </p>
+          <div className={styles.gallery}>
+            {products.map((product, index) => (
+              <Card key={index} {...product} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };

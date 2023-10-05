ce7f6d85d273abef2412ae1134f2477be5c07bee
MilanJulinac42 <milanjulinac996@gmail.com>
2023-10-05 19:19:51 +0200

    removed comments from hero

--- layout/hero/Hero.tsx
+++ layout/hero/Hero.tsx
@@ -4,15 +4,6 @@ import HeroImage from "../../components/heroImage/HeroImage";
 
 const Hero: React.FC = () => {
   return (
-    // <div className={styles.wrapper}>
-    // 	<h1 className={styles.heading}>Cinimo trenutke posebnim</h1>
-    // 	<button className={styles.cta}>SHOP NOW</button>
-    // 	<div className={styles.imageWrapper}>
-    // 		<HeroImage src="/gb1.jpg" />
-    // 		<HeroImage src="/gb2.jpeg" />
-    // 		<HeroImage src="/gb3.jpeg" />
-    // 	</div>
-    // </div>
     <div className={styles.wrapperT}>
       <div className={styles.contentWrapper}>
 		<p>regalo</p>

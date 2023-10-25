1b832c64e5a32128d4b053d4ab509062a3a1b4c7
MilanJulinac42 <milanjulinac996@gmail.com>
2023-10-25 15:02:45 +0200

    gb-93 added heading

---
+++ components/productHeading/ProductHeading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./ProductHeading.module.scss";
+
+const ProductHeading: React.FC = () => {
+  return (
+    <>
+      <h1>Gift Baskets</h1>
+    </>
+  );
+};
+
+export default ProductHeading;
--- pages/baskets.tsx
+++ pages/baskets.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import styles from "./baskets.module.scss";
+import styles from "./baskets/Baskets.module.scss";
 import Navbar from "../layout/navbar/Navbar";
 import Footer from "../layout/footer/Footer";
+import ProductHeading from "../components/productHeading/ProductHeading";
 
 const Baskets: React.FC = () => {
   return (
     <>
       <Navbar></Navbar>
+      <div className={styles.wrapper}>
+        <ProductHeading></ProductHeading>
+      </div>
       <Footer></Footer>
     </>
   );

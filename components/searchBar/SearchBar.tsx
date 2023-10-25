509ad9cee1714c12f607af8b420150cab57c8dc2
MilanJulinac42 <milanjulinac996@gmail.com>
2023-10-25 20:35:43 +0200

    gb-98 implemented hook

--- components/searchBar/SearchBar.tsx
+++ components/searchBar/SearchBar.tsx
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 import styles from "./SearchBar.module.scss";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 
 const SearchBar: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [productOptions, setProductOptions] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [showBasketBox, setShowBasketBox] = useState<boolean>(false);
+  const searchBarRef = useRef(null);
+
+  useOnClickOutside(searchBarRef, () => setShowBasketBox(false));
 
   const handleSelectClick = async () => {
     try {
@@ -16,6 +21,7 @@ const SearchBar: React.FC = () => {
         "http://localhost:9090/gift-basket/baskets-names"
       );
       setProductOptions(response.data.baskets);
+      setShowBasketBox(true)
       setIsLoading(false);
     } catch (error) {
       console.error("Error fetching product options", error);
@@ -31,7 +37,7 @@ const SearchBar: React.FC = () => {
   );
 
   return (
-    <div className={styles.searchBar}>
+    <div className={styles.searchBar} ref={searchBarRef}>
       <div className={styles.selectWrapper}>
         <input
           type="text"
@@ -44,13 +50,15 @@ const SearchBar: React.FC = () => {
           <FontAwesomeIcon icon={faSearch} />
         </span>
       </div>
-      <div className={styles.basketBoxWrapper}>
-        {filteredOptions.map((option, index) => (
-          <div key={index} className={styles.basketNameWrapper}>
-            {option.name}
-          </div>
-        ))}
-      </div>
+      {showBasketBox && (
+        <div className={styles.basketBoxWrapper}>
+          {filteredOptions.map((option, index) => (
+            <div key={index} className={styles.basketNameWrapper}>
+              {option.name}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };

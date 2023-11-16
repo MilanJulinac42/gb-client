c257151cba6e6e2adc3d18de1c5a773fb6948d7d
MilanJulinac42 <milanjulinac996@gmail.com>
2023-11-16 01:35:17 +0100

    gb-98 added sorting to baskets

--- pages/baskets.tsx
+++ pages/baskets.tsx
@@ -5,6 +5,7 @@ import Footer from "../layout/footer/Footer";
 import ProductHeading from "../components/productHeading/ProductHeading";
 import SearchBar from "../components/searchBar/SearchBar";
 import Filters from "../layout/filters/Filters";
+import Sorting from "../components/sorting/Sorting";
 
 const Baskets: React.FC = () => {
   return (
@@ -13,13 +14,11 @@ const Baskets: React.FC = () => {
       <div className={styles.container}>
         <ProductHeading></ProductHeading>
         <div className={styles.sortAndSearchWrapper}>
-          <SearchBar></SearchBar>
-          {/* sorting */}
-        </div>
-        <div className={styles.listAndFiltersWrapper}>
           <Filters />
-          {/* baskets list */}
+          <SearchBar></SearchBar>
+          <Sorting />
         </div>
+        <div className={styles.listAndFiltersWrapper}>{/* baskets list */}</div>
       </div>
       <Footer></Footer>
     </>

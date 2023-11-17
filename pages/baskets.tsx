b4c76e63989fd5c261ce4f560bc700b197e637ca
MilanJulinac42 <milanjulinac996@gmail.com>
2023-11-17 17:24:36 +0100

    bg-100 fixed layout

--- pages/baskets.tsx
+++ pages/baskets.tsx
@@ -6,6 +6,7 @@ import ProductHeading from "../components/productHeading/ProductHeading";
 import SearchBar from "../components/searchBar/SearchBar";
 import Filters from "../layout/filters/Filters";
 import Sorting from "../components/sorting/Sorting";
+import BasketCardList from "../layout/basketCardList/BasketCardList";
 
 const Baskets: React.FC = () => {
   return (
@@ -13,12 +14,19 @@ const Baskets: React.FC = () => {
       <Navbar></Navbar>
       <div className={styles.container}>
         <ProductHeading></ProductHeading>
-        <div className={styles.sortAndSearchWrapper}>
-          <Filters />
-          <SearchBar></SearchBar>
-          <Sorting />
+        <div className={styles.wrapper}>
+          <div className={styles.filtersWrapper}>
+            <Filters />
+          </div>
+          <div className={styles.test}>
+            <div className={styles.test2}>
+              <SearchBar />
+              <Sorting />
+            </div>
+            <BasketCardList />
+          </div>
         </div>
-        <div className={styles.listAndFiltersWrapper}>{/* baskets list */}</div>
+        <div className={styles.listAndFiltersWrapper}></div>
       </div>
       <Footer></Footer>
     </>

8407c9996493dea9db88b4fee21c79097eb11c5f
MilanJulinac42 <milanjulinac996@gmail.com>
2023-11-18 21:29:18 +0100

    gb-100 added custom hook to check window size

---
+++ hooks/useWindowSize.ts
@@ -0,0 +1,24 @@
+// useWindowSize.js
+import { useState, useEffect } from "react";
+
+const useWindowSize = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 992);
+  };
+
+  useEffect(() => {
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return { isMobile };
+};
+
+export default useWindowSize;
